eec8599bf94b0b177aac768b29e2f51f197f596f
albertlochmuller <albert.lochmueller@hubject.com>
2018-11-28 16:59:50 +0100

    removed unneccessary styles

--- index.tsx
+++ index.tsx
@@ -1,18 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {style} from 'typestyle';
-import cc from "classcat";
 import {CounterButton} from './src/CounterButton';
 
 const ROOT = document.querySelector('.container');
 
-const styles = {
-    // redColor: style({
-    //     color: 'red',
-    // }),
-    // backGroundGreen: style({
-    //     background: 'green'
-    // })
-};
+const styles = {};
 
-ReactDOM.render(<div className={cc([styles.redColor,  styles.backGroundGreen])}><CounterButton/></div>, ROOT);
+ReactDOM.render(<div><CounterButton/></div>, ROOT);
